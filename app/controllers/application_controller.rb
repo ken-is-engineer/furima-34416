6039c73f60d1f3431ad494aed5444b7b70ef482e
ken-is-engineer <kennoadoresu03@gmail.com>
2021-02-22 11:53:32 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth                                         #privateメソッド先にbasic認証を導入する記述
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER_2"] && password == ENV["BASIC_AUTH_PASSWORD_2"]  # 環境変数を読み込む記述に変更
+    end
+  end
 end
