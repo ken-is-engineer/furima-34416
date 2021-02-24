dca825d80f574215cf9ad5abc538b4a0037900f2
ken-is-engineer <kennoadoresu03@gmail.com>
2021-02-24 15:57:27 +0900

    controller作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,11 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    if user_signed_in?
+      @item = Item.new
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
@@ -13,5 +17,10 @@ class ItemsController < ApplicationController
 
   def edit
     
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :detail, :status_id, :fee_id, :from_id, :days_id, :category_id, :price).merge(user_id: current_user.id)
   end
 end
