7bb074e0ef89b2a84c144c04379b58c9ba20eec8
ken-is-engineer <kennoadoresu03@gmail.com>
2021-03-02 14:24:33 +0900

    JSファイル作成・controller,model,table作成

---
+++ app/javascript/card.js
@@ -0,0 +1,5 @@
+function pay() {
+  const form = document.getElementById()
+};
+
+window.addEventListener("load",pay);
--- app/models/item.rb
+++ app/models/item.rb
@@ -26,5 +26,6 @@ class Item < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_one    :purchase
 
 end
