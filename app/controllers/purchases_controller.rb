3792417885c41f49d15a6a14daf735a489a2add9
ken-is-engineer <kennoadoresu03@gmail.com>
2021-03-04 02:19:01 +0900

    formオブジェクトパターンによる購入情報DB登録の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,23 @@
 class PurchasesController < ApplicationController
   def index
-    
+    @purchase = Form.new
+    @item = Item.find(params[:id])
   end
-  
+
+  def create
+    @purchase = Form.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:id])
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:post_code, :prefecture_id, :city, :address_line, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:id])
+  end
 end
---
+++ app/models/form.rb
@@ -0,0 +1,24 @@
+class Form
+  include ActiveModel::Model
+  attr_accessor :token, :post_code, :prefecture_id, :city, :address_line, :building, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :post_code,     format:{with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :city
+    validates :address_line
+    validates :phone_number,  format:{with: /\A[0-9]+\z/}
+
+    validates :user_id
+    validates :item_id
+
+    #validates :building は無い場合があるためバリデーション不要。
+    #user_id,item_idは本来アソシエーションでバリデーションも兼ねているが、formオブジェクトパターンではアソシエーションできないので追加する。
+  end
+
+  # 各テーブルにデータを保存する処理を書く
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building: building, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
