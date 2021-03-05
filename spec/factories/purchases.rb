8d735a197b1b74f0fe8e1de1d549e3d88e472416
ken-is-engineer <kennoadoresu03@gmail.com>
2021-03-05 18:19:27 +0900

    単体テスト（purchaseモデル）

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
-  factory :purchase do
-    
+  factory :form do
+    post_code   {'000-0000'}
+    prefecture_id {rand(2..10)}
+    city          {Faker::Address.city}
+    address_line  {Faker::Address.street_address}
+    building      {Faker::Address.secondary_address}
+    phone_number  {rand(10000000000..99999999999)}
+    user_id       {1}
+    item_id       {1}
+    token         {'tok_abcdefghijk00000000000000000'}
   end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Form, type: :model do
+  before do
+    @purchase = FactoryBot.build(:form)
+  end
+
+  describe 'purchase an item' do
+    context 'Success to purchase' do
+      it 'Success to purchase when you input correctly' do
+        expect(@purchase).to be_valid
+      end
+    end
+
+    context 'Fail to purchase' do
+      it 'Fail to purchase when token is blank' do
+        @purchase.token = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Token can't be blank")
+      end
+      it 'Fail to purchase when post_code is blank' do
+        @purchase.post_code = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+      it 'Fail to purchase when postal_code doesnt include hyphen' do
+        @purchase.post_code = '1111111'
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+      it 'Fail to purchase when prefecture is not chosen' do
+        @purchase.prefecture_id = 1
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'Fail to purchase when city is blank' do
+        @purchase.city = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("City can't be blank")
+      end
+      it 'Fail to purchase when address_line is blank' do
+        @purchase.address_line = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Address line can't be blank")
+      end
+      it 'Fail to purchase when phone_number is blank' do
+        @purchase.phone_number = ''
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'Fail to purchase when phone_number is not number' do
+        @purchase.phone_number = '000-9999-88'
+        @purchase.valid?
+        expect(@purchase.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+  end
 end
